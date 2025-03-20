46836d0ec98b1577e2ce9aa15ec52b25a95df60a
Meenu-vis <meenukumar@vis10dwarka.com>
2025-03-20 23:33:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
-from google.colab import drive
 
 # Load the dataset
 data = pd.read_csv('diabetes.csv')
